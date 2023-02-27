75007f23815d27df6078aa0a49ecffd540e2042a
fernanda0124a <99252435+fernanda0124a@users.noreply.github.com>
2023-02-27 10:55:00 -0300

    Add files via upload

---
+++ Primeiro Projeto/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php echo "Ola mundo";   ?></h1>
+    <h1><?php echo "2 + 2 =" ?></h1>
+    <h2><?php echo (2+2) ?></h2>
+    <h1><?php echo "5 - 3 =" ?></h1>
+    <h2><?php echo (5-3) ?></h2>
+    <h1><?php echo "10 * 10 =" ?></h1>
+    <h2><?php echo (10*10) ?></h2>
+    <h1><?php echo "2/2 =" ?></h1>
+    <h2><?php echo (2/2) ?></h2>
+
+</body>
+</html>
